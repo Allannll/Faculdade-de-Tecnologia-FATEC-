3ddcfcd42a3fe80d198f8330305bf9a8a9b51b14
Allan Gabriel <162365080+Allannll@users.noreply.github.com>
2024-10-16 23:10:24 -0300

    Add files via upload

---
+++ 1º Semester/Computer Architecture and Organization/Conversor/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        TextBox1 = New TextBox()
+        ComboBox1 = New ComboBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        ComboBox2 = New ComboBox()
+        Label3 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(339, 344)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Executar"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(88, 72)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 1
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Binário", "Decimal", "Hexadecimal"})
+        ComboBox1.Location = New Point(385, 72)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(121, 23)
+        ComboBox1.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(88, 54)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(35, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "Input"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(385, 54)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(77, 15)
+        Label2.TabIndex = 4
+        Label2.Text = "Base de saída"
+        ' 
+        ' ComboBox2
+        ' 
+        ComboBox2.FormattingEnabled = True
+        ComboBox2.Items.AddRange(New Object() {"Binário", "Decimal", "Hexadecimal"})
+        ComboBox2.Location = New Point(225, 72)
+        ComboBox2.Name = "ComboBox2"
+        ComboBox2.Size = New Size(121, 23)
+        ComboBox2.TabIndex = 5
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(225, 54)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(90, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Base de entrada"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Label3)
+        Controls.Add(ComboBox2)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ 1º Semester/Computer Architecture and Organization/Conversor/Funções.vb
@@ -0,0 +1,90 @@
+﻿Imports System.Math
+Imports System.Numerics
+Public Class Funções
+
+    Private Function ConvertAnyToDec(ByVal input, ByVal original_base)
+
+        Dim number As String
+        Dim i As BigInteger = 0
+        Dim soma As BigInteger
+
+
+        number = InvertString(input)
+
+        While i < Len(number)
+
+            Dim letter As Char = number.Substring(i, 1)(0)
+            Dim digitvalue As BigInteger
+
+            If Char.IsDigit(letter) Then
+                digitvalue = Asc(letter) - Asc("0"c)
+            ElseIf Char.IsLetter(letter) Then
+                digitvalue = Asc(Char.ToUpper(letter)) - Asc("A"c) + 10
+            End If
+
+            soma += BigInteger.Pow(original_base, i) * digitvalue
+            i += 1
+        End While
+
+        Return soma.ToString()
+
+        'MsgBox(soma)
+
+    End Function
+
+    Private Function ConvertDecToAny(ByVal input, ByVal target)
+
+        Dim number As BigInteger
+        Dim resto As BigInteger
+        Dim result As String
+
+        number = BigInteger.Parse(input)
+
+        While number > 0
+
+            resto = number Mod target
+
+            Dim letter As Char
+
+            If resto >= 0 AndAlso resto <= 9 Then
+                letter = Chr(Asc("0"c) + resto)
+            ElseIf resto >= 10 AndAlso resto <= 35 Then
+                letter = Chr(Asc("A"c) + (resto - 10))
+            End If
+
+            result = letter & result
+
+            number /= target
+
+        End While
+
+        Return result
+
+    End Function
+
+    Public Function AnyToAny(ByVal input, ByVal original_base, ByVal target)
+
+        Dim inDecimal As String = ConvertAnyToDec(input, original_base)
+
+        If target = 10 Then
+            Return inDecimal
+        Else
+            Return ConvertDecToAny(inDecimal, target)
+        End If
+
+    End Function
+
+
+    Private Function InvertString(ByVal value As String)
+
+        Dim CharArray() As Char = value.ToCharArray()
+
+        Array.Reverse(CharArray)
+
+        Dim reverseString As String = New String(CharArray)
+
+        Return reverseString
+
+    End Function
+
+End Class
